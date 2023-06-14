52c8ab583ac3555fe6c860b065f6b9f6e0b2aa4b
sudeb <sudebsarkar21@gmail.com>
2023-06-14 16:02:23 +0530

    Initial commit

---
+++ KnowYourSocials/KnowYourSocials/KnowYourSocialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowYourSocialsApp.swift
+//  KnowYourSocials
+//
+//  Created by Sudeb on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct KnowYourSocialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
